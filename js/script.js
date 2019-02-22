5d6c992838bc388e78a4ae248bdc71d4330129de
Sylwia Dras <sylwiadras99@gmail.com>
2019-02-22 11:40:05 +0100

    Add computerMove and playerMove vars

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var computerMove, playerMove;
+
+
+playerMove = 'papier';
+printMessage(('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!'));
+
+computerMove = 'kamień';
+printMessage(('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'));
