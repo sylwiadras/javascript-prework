bc3c7729ea1276137d64896212eaea62ea320c76
Sylwia Dras <sylwiadras99@gmail.com>
2019-02-22 12:16:37 +0100

    Add randomize numbers, assign rock, paper, scissors

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-var computerMove, playerMove;
+var computerMove, randomNumber;
 
+randomNumber = Math.floor(Math.random() * 3 + 1);
 
-playerMove = 'papier';
-printMessage(('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!'));
+console.log(('wylosowana liczba to: ' + randomNumber));
 
-computerMove = 'kamień';
-printMessage(('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'));
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+}
+
+if (randomNumber == '2') {
+    computerMove = 'papier';
+}
+
+if (randomNumber == '3') {
+    computerMove = 'nożyce';
+}
+
+printMessage (('Mój ruch to: ' + computerMove));
